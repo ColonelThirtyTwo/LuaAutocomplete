0310bfcdc4b399a28b41aa7b638e4e51decb5cdd
Alex Parrill <initrd.gz@gmail.com>
2014-07-30 16:27:17 -0500

    Added require autocompletion.

--- LuaAutocomplete.py
+++ LuaAutocomplete.py
@@ -1,32 +1,34 @@
 
 import sublime, sublime_plugin
+import re, os, itertools
 from LuaAutocomplete.locals import LocalsFinder
 
-def can_local_autocomplete(view, location):
-	"""
-	Returns true if locals autocompetion makes sense in the specified location (ex. its not indexing a variable, in a string, ...)
-	"""
-	pos = view.find_by_class(location, False, sublime.CLASS_WORD_START)
-	if pos == 0:
+class LocalsAutocomplete(sublime_plugin.EventListener):
+	@staticmethod
+	def can_local_autocomplete(view, location):
+		"""
+		Returns true if locals autocompetion makes sense in the specified location (ex. its not indexing a variable, in a string, ...)
+		"""
+		pos = view.find_by_class(location, False, sublime.CLASS_WORD_START)
+		if pos == 0:
+			return True
+		
+		scope_name = view.scope_name(location)
+		if "string." in scope_name or "comment." in scope_name:
+			# In a string or comment
+			return False
+		
+		if "parameter" in scope_name:
+			# Specifying parameters
+			return False
+		
+		char = view.substr(pos-1)
+		if char == "." or char == ":":
+			# Indexing a value
+			return False
+		
 		return True
 	
-	scope_name = view.scope_name(location)
-	if "string." in scope_name or "comment." in scope_name:
-		# In a string or comment
-		return False
-	
-	if "parameter" in scope_name:
-		# Specifying parameters
-		return False
-	
-	char = view.substr(pos-1)
-	if char == "." or char == ":":
-		# Indexing a value
-		return False
-	
-	return True
-
-class LuaAutocomplete(sublime_plugin.EventListener):
 	def on_query_completions(self, view, prefix, locations):
 		if view.settings().get("syntax") != "Packages/Lua/Lua.tmLanguage":
 			# Not Lua, don't do anything.
@@ -34,7 +36,7 @@ class LuaAutocomplete(sublime_plugin.EventListener):
 		
 		location = locations[0] # TODO: Better multiselect behavior?
 		
-		if not can_local_autocomplete(view, location):
+		if not LocalsAutocomplete.can_local_autocomplete(view, location):
 			return
 		
 		src = view.substr(sublime.Region(0, view.size()))
@@ -43,3 +45,48 @@ class LuaAutocomplete(sublime_plugin.EventListener):
 		varz = localsfinder.run(location)
 		
 		return [(name+"\t"+data.vartype,name) for name, data in varz.items()], sublime.INHIBIT_WORD_COMPLETIONS
+
+class RequireAutocomplete(sublime_plugin.EventListener):
+	
+	@staticmethod
+	def filter_lua_files(filenames):
+		for f in filenames:
+			fname, ext = os.path.splitext(f)
+			if ext == ".lua" or ext == ".luac":
+				yield fname
+	
+	def on_query_completions(self, view, prefix, locations):
+		if view.settings().get("syntax") != "Packages/Lua/Lua.tmLanguage":
+			# Not Lua, don't do anything.
+			return
+		
+		proj_file = view.window().project_file_name()
+		if not proj_file:
+			# No project
+			return
+		
+		location = locations[0]
+		src = view.substr(sublime.Region(0, location))
+		
+		match = re.search(r"""require\s*\(?\s*["']([^"]*)$""", src)
+		if not match:
+			return
+		
+		module_path = match.group(1).split(".")
+		
+		results = []
+		proj_dir = os.path.dirname(proj_file)
+		
+		for proj_subdir in view.window().project_data()["folders"]:
+			proj_subdir = proj_subdir["path"]
+			cur_path = os.path.join(proj_dir, proj_subdir, *(module_path[:-1]))
+			print("curpath:", cur_path)
+			if not os.path.exists(cur_path) or not os.path.isdir(cur_path):
+				continue
+			
+			_, dirs, files = next(os.walk(cur_path)) # walk splits directories and regular files for us
+			
+			results.extend(map(lambda x: (x+"\tsubdirectory", x+"."), dirs))
+			results.extend(map(lambda x: (x+"\tmodule", x), RequireAutocomplete.filter_lua_files(files)))
+		
+		return results, sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS
--- readme.md
+++ readme.md
@@ -6,6 +6,13 @@ This is a very basic auto-completion plugin for Sublime Text 3 and the Lua langu
 It's meant to be an improvement over Sublime Text's default autocomplete, which is frequently
 cluttered with noise from comments, etc.
 
-Only local variables are autocompleted at the moment. This includes function parameters and for
-loop indices, but not globals or table contents (as those are hard to detect for dynamically-typed
-languages).
+Types of autocompletion added:
+
+* Local variables, including function parameters, for loop indices, etc.
+* File paths in `require`. This will search along all folders added to the project for Lua files and directories,
+and adds them as autocompletion entries when calling the `require` function.
+
+Wishlist:
+
+* Builtins autocompletion.
+* Table member completion (really hard!).
